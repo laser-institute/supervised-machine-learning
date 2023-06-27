3efc655860c0802675dfcf535c89ecc70f2f474b
jrosen48 <jmrosen48@gmail.com>
2023-06-26 22:49:35 -0400

    add worsley, work on ll3 and ll4 case studies

--- lab-3/ll-3-case-study-scratch.R
+++ lab-3/ll-3-case-study-scratch.R
@@ -17,17 +17,6 @@ code_module_dates <- assessments %>%
     group_by(code_module, code_presentation) %>% 
     summarize(quantile_cutoff_date = quantile(date, probs = .25, na.rm = TRUE))
 
-assessments_joined <- assessments %>% 
-    left_join(code_module_dates) # join the data based on course_module and course_presentation
-
-assessments_filtered <- assessments_joined %>% 
-    filter(date < quantile_cutoff_date) # filter the data so only assignments before the cutoff date are included
-
-assessments_summarized <- assessments_filtered %>% 
-    mutate(weighted_score = score * weight) %>% # create a new variable that accounts for the "weight" (comparable to points) given each assignment
-    group_by(id_student) %>% 
-    summarize(mean_weighted_score = mean(weighted_score)) 
-
 ## ---------------------------------------------------------------------------------------------------
 
 # students and assessments 
@@ -150,8 +139,6 @@ fitted_model_resamples %>%
 ## ---------------------------------------------------------------------------------------------------
 class_metrics <- metric_set(accuracy, sensitivity, specificity, ppv, npv, kap) # add probs?
 
-
-## ---------------------------------------------------------------------------------------------------
 fitted_model <- fit(my_wf, data_train)
 final_fit <- last_fit(fitted_model, train_test_split, metrics = class_metrics)
 
