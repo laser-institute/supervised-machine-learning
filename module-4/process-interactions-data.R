19f5251a9f27ecbbf823855b59a67e917979e68b
Joshua Rosenberg <jmrosen48@gmail.com>
2025-07-13 16:19:04 -0400

    update mod 4 to address the wtf issues

--- module-4/process-interactions-data.R
+++ module-4/process-interactions-data.R
@@ -1,16 +1,12 @@
 library(tidyverse)
+library(arrow)
 
-interactions <- read_csv("data/oulad-interactions.csv")
-assessments <- read_csv("data/oulad-assessments.csv")
+# this data is ignored in .gitignore
+# the raw data is available from: https://analyse.kmi.open.ac.uk/
+interactions <- read_csv("module-4/data/oulad-interactions.csv")
+vle <- read_csv("module-4/data/oulad-vle.csv")
 
-code_module_dates <- assessments %>% 
-  group_by(code_module, code_presentation) %>% 
-  summarize(quantile_cutoff_date = quantile(date, probs = .33, na.rm = TRUE)) # change this throughout
+interactions <- interactions %>% 
+    left_join(vle, by = c("id_site", "code_module", "code_presentation"))
 
-students_and_assessments <- read_csv("data/oulad-students-and-assessments.csv")
-
-interactions_joined <- interactions %>% 
-    left_join(code_module_dates) # join the data based on course_module and course_presentation
-
-interactions_filtered <- interactions_joined %>% 
-    filter(date < quantile_cutoff_date) # filter the data so only assignments before the cutoff date are included
+write_parquet(interactions, "module-4/data/oulad-interactions.parquet")
